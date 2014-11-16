a5d19cdc9453773ac5a1da4a1bc77ff1a5cc7ffc
ColonelFluff <gunnyarts@gmail.com>
2014-11-16 21:32:01 +0100

    styled header

--- includes/header_inc.php
+++ includes/header_inc.php
@@ -4,13 +4,15 @@
 <body>
 	<main>
 		<header>
-			<a href="index.php"><img src="images/logo.png" alt="Dykon gås logo"></a>
-			<a href="index.php"><img src="images/dykon.png" alt="Dykon logo"></a>
-			
-			<form action="./results.php" method="GET" id="header-search">
-				<input type="text" name="input" size="50" placeholder="Search...." />
-				<input type="submit" value="search" />
-			</form>
+			<a href="index.php"><img src="images/logo.png" alt="Dykon gås logo" class="goose-logo"></a>
+			<a href="index.php"><img src="images/dykon.png" alt="Dykon logo" class="dykon-logo"></a>
+			<section class="header-right">
+				<a href="#">EN</a>
+				<a href="#">DE</a>
+				<form action="./results.php" method="GET" class="header-search">
+					<input type="text" name="input" placeholder="Search...." class="search-bar" />
+				</form>
+			</section>
 <?php include 'includes/menu_inc.php'; ?>
 			
 		</header>
