f3d71ea6ae8aaa07f46baa2b609c336a38d66ca5
lort101 <christinaholdensen@gmail.com>
2014-11-16 17:59:15 +0100

    tekst på partneresiderne

--- partnere.php
+++ partnere.php
@@ -21,9 +21,9 @@
 	<nav>
 		<ul>
 			<li><a href="ringsted-dun.php"> <img src="images/ringsted-dun-logo copy.jpg" alt="Ringsted Dun"> </a></li>
-			<li><a href="flora-danica.php"> <img src="images/flora-danica-logo.jpg" alt="Flora Danica"> </a></li>
 			<li><a href="danica.php"> <img src="images/danica-logo.jpg" alt="Danica"> </a></li>
 			<li><a href="sirius-collection.php"> <img src="images/sirius-logo.jpg" alt="Sirius Collection"> </a></li>
+            <li><a href="flora-danica.php"> <img src="images/flora-danica-logo.jpg" alt="Flora Danica"> </a></li>
 			<li><a href="engmo-dun.php"> <img src="images/engmo-logo.jpg" alt="Egmo Dun"> </a></li>
 			<li><a href="bergen-dun.php"> <img src="images/bergen-dun-logo.jpg" alt="Bergen Dun"></a></li>
 		</ul>
--- ringsted-dun.php
+++ ringsted-dun.php
@@ -10,9 +10,16 @@
 	<!------------------------------>
 	<!-- INSERT PAGE CONTENT HERE -->
 	<!------------------------------>
-	<section>
+	<section class="partnere">
     
-    <h1>ringsted dun</h1>
+     <article class="text-box-ringsted">
+    	<h2>Ringsted Dun</h2>
+   		<p> Det er vigtigt for alle mennesker at sove godt. De fleste af os har erfaret, at der er en direkte sammenhæng mellem kvaliteten af vores sengemiljø, og hvor udhvilet vi føler os, når vi vågner. </p>
+        <p>Derfor bør du altid være meget kritisk, når du beslutter hvilken dyne og pude, du ønsker at tilbringe dine nætter sammen med.</p>
+		<p>Hvis du vælger Ringsted Dun, kan du sikre dig en god søvn mange år frem i tiden.</p>
+		<p>Vi er ikke kommet sovende til vores succes. Siden 1947 har Ringsted Dun arbejdet med at udvikle og producere dyner og puder. Og i dag er vores navn blevet en international mærkevare kendt for kvalitet.</p>
+    </article>
+    
 	</section>
  <?php include 'includes/footer.php'; ?>
 	<!-- INSERT PLUGIN JAVASCRIPT HERE -->
