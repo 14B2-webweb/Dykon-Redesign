608017ad91532febe283f1cd1dba7d01b4c99ef1
ColonelFluff <gunnyarts@gmail.com>
2014-11-17 13:21:26 +0100

    updated header

--- includes/header_inc.php
+++ includes/header_inc.php
@@ -4,8 +4,12 @@
 <body>
 	<main>
 		<header>
-			<a href="index.php"><img src="images/logo.png" alt="Dykon gås logo" class="goose-logo"></a>
-			<a href="index.php"><img src="images/dykon.png" alt="Dykon logo" class="dykon-logo"></a>
+			<section class="header-left">
+				<a href="index.php"><img src="images/logo.png" alt="Dykon gås logo" class="goose-logo"></a>
+			</section>
+			<section class="header-center">
+				<a href="index.php"><img src="images/dykon.png" alt="Dykon logo" class="dykon-logo"></a>
+			</section>
 			<section class="header-right">
 				<a href="#">EN</a>
 				<a href="#">DE</a>
