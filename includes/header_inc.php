28ffa06c6c23330fd2a53d5b010dc86898c7067c
lort101 <christinaholdensen@gmail.com>
2014-11-17 14:19:29 +0100

    fixed?

--- includes/header_inc.php
+++ includes/header_inc.php
@@ -4,17 +4,14 @@
 <body>
 	<main>
 		<header>
-<<<<<<< HEAD
 			<a href="index.php"><img src="images/logo.png" alt="Dykon gås logo" class="goose-logo"></a>
 			<a href="index.php"><img src="images/dykon.png" alt="Dykon logo" class="dykon-logo"></a>
-=======
 			<section class="header-left">
 				<a href="index.php"><img src="images/logo.png" alt="Dykon gås logo" class="goose-logo"></a>
 			</section>
 			<section class="header-center">
 				<a href="index.php"><img src="images/dykon.png" alt="Dykon logo" class="dykon-logo"></a>
 			</section>
->>>>>>> master
 			<section class="header-right">
 				<a href="#">EN</a>
 				<a href="#">DE</a>
