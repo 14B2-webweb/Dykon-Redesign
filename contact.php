d6fd62e621c22138e0c56d68653cb070a116f9aa
ColonelFluff <gunnyarts@gmail.com>
2014-11-21 11:00:12 +0100

    updated contact.php

--- contact.php
+++ contact.php
@@ -10,15 +10,13 @@
 	<!------------------------------>
 
 	<section class="contact">
-			<h1>Kontakt side</h1>
+			<h1>Kontakt</h1>
 			
             
             <section class="contactus">
-            <h2>Kontakt</h2>
-
-                    <p>Ønsker du oplysninger om DYKONs landsdækkende forhandlernet, har                     du spørgsmål eller kommentarer i øvrigt, er du velkommen til at                         kontakte os.</p>
+                    <br><br><p>Ønsker du oplysninger om DYKON's landsdækkende forhandlernet, har du spørgsmål eller kommentarer i øvrigt, er du velkommen til at                         kontakte os.</p>
                 
-            <h2>DYKON A/S</h2>
+            <h2 class="contact">DYKON A/S</h2>
                 <p>
                 Kongsbjerg 15<br>
                 6640 Lunderskov<br>
@@ -34,7 +32,7 @@
             </section>
             
             <section class="contactform">
-                <h2>Kontakt formular</h2>
+            <h2 class="contact">Kontakt formular</h2>
             <form class="contactform" action="contactformular.php" method="post"/>
             <p>
             Navn:
@@ -58,7 +56,8 @@
             </p>
             
             <br><p class="contactform">Besked</p>
-            <textarea class="box" name="cf_message" rows="10" cols="38"></textarea><br>
+            
+            <textarea name="cf_message" rows="10" cols="44"></textarea><br>
                 
             <input class="button" type="submit" value="Send"/>
             <input class="button" type="reset" value="Slet"/>
@@ -66,9 +65,8 @@
             </section>
                 
             <section class="googlemaps">
-            <h2>Navigation</h2>
-            <br>
-            <div id="map_canvas"></div>
+            <h2 class="contact">Navigation</h2>
+            <iframe class="navigation" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2260.6001275864437!2d9.292219999999999!3d55.487066999999996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464b5e1b0c0e723d%3A0xa2775a023fd1fdac!2sKongsbjerg+15%2C+6640+Lunderskov!5e0!3m2!1sda!2sdk!4v1416489727706" width="280" height="350" frameborder="0" style="border:0"></iframe>
             </section>
 	</section
  <?php include 'includes/footer.php'; ?>
